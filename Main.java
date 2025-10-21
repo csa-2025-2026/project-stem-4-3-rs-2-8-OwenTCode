14ee258c5da06a13830532aab3cc28a1bad4ac13
OwenTCode <Oweneight88@gmail.com>
2025-10-21 18:22:22 +0000

    prob 2 in prog

--- Main.java
+++ Main.java
@@ -5,5 +5,19 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Problem 1
+
+    //Problem 2
+    int count = ;
+
+    for (int num = 16; num < 73; count++)
+    {
+      num++
+      System.out.print(num);
+      if (count%11 == 0)
+
+    }
+    
+
   }
 }
