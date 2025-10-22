8587e0153465af89f41a07354d870c713f85671c
OwenTCode <Oweneight88@gmail.com>
2025-10-22 18:05:27 +0000

    wdads

--- Main.java
+++ Main.java
@@ -8,16 +8,36 @@ public class Main
     //Problem 1
 
     //Problem 2
-    int count = ;
+    int count = 1;
 
     for (int num = 16; num < 73; count++)
     {
-      num++
+      num++;
       System.out.print(num);
-      if (count%11 == 0)
+      System.out.print(" ");
+      if (count%10 == 0)
+      {
+        System.out.println("");
+      }
+
 
     }
+    System.out.println("");
     
+    //Problem 3
+    Scanner sc = new Scanner(System.in);
+    int blah = sc.nextInt();
+
+    while (blah <= 50)
+    {
+      System.out.print(blah + " ");
+      blah++;
+      if (blah < 0)
+      {
+        System.out.println("error");
+        return blah;
+      }
+    }
 
   }
 }
